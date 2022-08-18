abbb5fa7b2563e86f83ef601268b448b2c390966
André Souza <andrecs.06@gmail.com>
2022-08-17 22:00:23 -0300

    New file comparacao

---
+++ comparacao.js
@@ -0,0 +1,14 @@
+// == (comparação implicita)
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero == texto);
+
+//== só compara o valor
+//=== compara o valor e o tipo do dado
+
+//conversão explicita 
+
+Number();
+String();
