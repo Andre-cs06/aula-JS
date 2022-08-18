d97ee74df7ada568bb546dbdb620d6406735fc64
André Souza <andrecs.06@gmail.com>
2022-08-17 22:40:50 -0300

    new file tenario

---
+++ ternario.js
@@ -0,0 +1,10 @@
+const idedeMinima = 18;
+const idadeCliente = 23;
+
+// if(idadeCliente >= idedeMinima) {
+//     console.log("Cerveja");
+// }else{
+//     console.log("suco");
+// }
+
+console.log(idadeCliente >= idedeMinima ? "Cerveja" : "Suco");
