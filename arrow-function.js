d6f4fd16acb60c387ce75bbd7a8fbfefdf3fd78e
André Souza <andrecs.06@gmail.com>
2022-08-25 20:47:49 -0300

    new folder

---
+++ arrow-function.js
@@ -0,0 +1,22 @@
+function apresentar(nome) {
+    return`meu nome é ${nome}`;
+}
+
+//Arrow function
+const apresentarArrow = nome => `meu nome é ${nome}`;
+const soma = (num1, num2) => num1 + num2;
+
+// console.log(apresentar("André"));
+//console.log(soma(10, 54));
+
+//Arrow funcion com mais de uma linha 
+
+const somaNumeros = (num1, num2) => {
+    if (num1 >10 || num2 > 10) {
+        return"somente números menores que nove";
+    }else{
+        return num1 + num2
+    }
+}
+
+console.log(somaNumeros(5, 7));
