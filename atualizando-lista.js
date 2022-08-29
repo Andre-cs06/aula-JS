a08c18b22609ffdadb6cc8d0fc10fdfb067b92b6
André Souza <andrecs.06@gmail.com>
2022-08-28 22:15:20 -0300

    new file

---
+++ atualizando-lista.js
@@ -0,0 +1,8 @@
+const listaDeChamada = ['João', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Leo']
+
+// listaDeChamada.splice(1,2,'Rodrigo') remove ana e caio e adiciona Rodrigo no lugar 
+listaDeChamada.splice(2,0,'Rodrigo') //adiciona Rodrigo sem remover ninguem 
+
+// console.log(listaDeChamada)
+
+console.log(`Nova lista atualizada: ${listaDeChamada}`)
---
+++ juntando-turmas.js
@@ -0,0 +1,6 @@
+const salaPython = ['Melissa', 'Helena', 'Rofrigo']
+const salaJavaScript = ['Juliana', 'Leonardo', 'André']
+
+const salaUnificada = salaPython.concat(salaJavaScript)
+
+console.log(salaUnificada)
